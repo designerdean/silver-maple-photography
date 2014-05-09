d050903fbde965f3dfd276714bbc7a37ed0fc61b
Josh Dean <josh@designerdean.com>
2014-05-08 21:15:51 -0600

    enabling s3_sync

--- config.rb
+++ config.rb
@@ -136,11 +136,11 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-# activate :s3_sync do |s3_sync|
-#   s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-#   s3_sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-#   s3_sync.bucket = ENV['S3_BUCKET']
-# end
+activate :s3_sync do |s3_sync|
+  s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.bucket = ENV['S3_BUCKET']
+end
 
 # activate :cloudfront do |cf|
 #   cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
